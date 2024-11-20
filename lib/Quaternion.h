e8d3f1532467d1f4ef9164c5663175745e7a28fc
Bartlomiej-Ejzert-247652 <247652@edu.p.lodz.pl>
2024-11-20 16:02:37 +0100

    quaternion add

---
+++ lib/Quaternion.h
@@ -0,0 +1,16 @@
+#ifndef ZADANIE1_QUATERNION_H
+#define ZADANIE1_QUATERNION_H
+
+class Quaternion {
+
+private:
+
+    float a, x, y, z;
+
+public:
+
+    Quaternion(float _a, float _x, float _y, float _z);
+
+};
+
+#endif //ZADANIE1_QUATERNION_H
---
+++ src/Quaternion.cpp
@@ -0,0 +1,11 @@
+#include "Quaternion.h"
+
+Quaternion::Quaternion(float _a, float _x, float _y, float _z) {
+
+    this -> a = _a;
+    this -> x = _x;
+    this -> y = _y;
+    this -> z = _z;
+
+}
+
